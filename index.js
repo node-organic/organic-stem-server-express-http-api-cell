02958c02dbfef9bf3790ca412940356864549dba
outbounder <obiwonn@gmail.com>
2018-04-06 13:07:21 +0300

    update to latest organic-stack-upgrade and depend on organic-stem-core-template due the need of monorepo commands

--- index.js
+++ index.js
@@ -2,28 +2,32 @@
 
 const StackUpgrade = require('organic-stack-upgrade')
 const path = require('path')
-const exec = require('util').promisify(require('child_process').exec)
 
-const execute = async function ({destDir = process.cwd(), answers}) {
+const execute = async function ({destDir = process.cwd(), answers} = {}) {
   let stack = new StackUpgrade({
     destDir: destDir,
     name: 'organic-stem-server-http-api-cell-template',
     version: '1.0.0'
   })
-  let resulted_answers = await stack.configure({
+  let coreTemplateExists = false
+  try {
+    coreTemplateExists = await stack.checkUpgrade('organic-stem-core-template', '^1.0.0')
+  } catch (e) {
+    // ignore any errors
+    coreTemplateExists = false
+  }
+  if (!coreTemplateExists) {
+    console.warn('organic-stem-core-template not found...')
+    console.info('force installing organic-angel, angelscripts-help and angelscripts-monorepo')
+    await stack.exec('npm install organic-angel angelscripts-help angelscripts-monorepo --save-dev')
+  }
+  let resulted_answers = await stack.configureMergeAndUpdateJSON({
     sourceDir: path.join(__dirname, 'seed'),
     answers
   })
-  await stack.merge({
-    sourceDir: path.join(__dirname, 'seed'),
-    answers: resulted_answers
-  })
-  await stack.updateJSON()
-  let cellDir = path.join(destDir, 'cells', answers['cell-name'])
-  console.info('run npm install...')
-  let npmOutput = await exec('npm install', {cwd: cellDir})
-  console.info(npmOutput.stdout)
-  console.error(npmOutput.stderr)
+  let cellName = resulted_answers['cell-name']
+  console.info(`run npm install on ${cellName}...`)
+  await stack.exec(`npx angel repo cell ${cellName} -- npm install`)
 }
 
 if (module.parent) {
